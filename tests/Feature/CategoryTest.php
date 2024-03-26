1cf54fd8f09b7a616fdf9c48e46ac3ec31882e92
omaimaelbaz <oumacode01.com>
2024-03-26 16:37:30 +0100

    test category

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\CategoryController;
+use App\Models\Categories;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CategoryTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+
+
+     public function testebdpointShow(): void
+     {
+        $categories = Categories::all();
+        $response = $this->getJson('/api/index');
+        // dd($response);
+        $response->assertJson($categories->toArray());
+     }
+}
