5b5b06c1dff3c7c10cb5c11dcd2a3270afd0dd21
omaimaelbaz <oumacode01.com>
2024-03-28 11:00:49 +0100

    add user test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+    public function test_user_can_be_created()
+    {
+
+        $response = $this->postJson('/api/users', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(201);
+        $this->assertCount(1, User::all());
+    }
+}
+
